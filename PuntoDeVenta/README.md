44745ac7dff90596333f94e8bcb376982bfd800f
GOrtiz04 <gabyortiz2013@gmail.com>
2023-09-18 22:25:04 -0600

    Version con funciones

--- PuntoDeVenta/README.md
+++
@@ -1,2 +0,0 @@
-# PuntoDeVenta
-ProyectoFinalSextoSemestre
--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,24 @@ const { Router } = require('express');
 const router = Router();
 
 // facturas
-/*const libroControler = require('../controllers/libro/libroControler');
-const generoControler = require('../controllers/libro/generoControler');*/
+const sucursalController = require('../controllers/1SucursalesController');
+const clienteController = require('../controllers/3ClientesController');
 
 //RUTAS
 
 module.exports = (app) => {
-    /*//libros
-    router.get('/libro/find', libroControler.find);
-    router.post('/libro/create', libroControler.create);
-    router.put('/libro/update', libroControler.update);
-    router.delete('/libro/delete/:id', libroControler.delete);
+    
+    //Sucursales
+    router.get('/sucursales/find', sucursalController.find);
+    router.post('/sucursales/create', sucursalController.create);
+    router.put('/sucursales/update', sucursalController.update);
+    //router.delete('/sucursales/delete/:id', sucursalControler.delete);
 
-    //generos
-    router.get('/genero/find', generoControler.findgeneros);
-    router.post('/genero/create', generoControler.create);
-    router.put('/genero/update', generoControler.update);
-    router.delete('/genero/delete/:id', generoControler.delete);*/
-
+    //Clientes
+    router.get('/clientes/find', clienteController.find);
+    router.post('/clientes/create', clienteController.create);
+    router.put('/clientes/update', clienteController.update);
+    //router.delete('/sucursales/delete/:id', sucursalControler.delete);
 
     app.use('/', router);
 
