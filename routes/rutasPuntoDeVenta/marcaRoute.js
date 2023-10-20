861e62723bfa05601bce805d00da8e2ce99d20a9
LudvinBaquiax56 <vasqueznicolas56@gmail.com>
2023-10-20 04:15:39 -0600

    Primer get por parametros

--- routes/rutasPuntoDeVenta/marcaRoute.js
+++ routes/rutasPuntoDeVenta/marcaRoute.js
@@ -5,6 +5,7 @@ const marcaController = require('../../controllers/puntoDeVenta/marcaController'
 
     router.get('/find', marcaController.find);
     router.get('/findById', marcaController.findById);
+    router.get('/findById/:id', marcaController.findById2);
     router.post('/create', marcaController.create);
     router.put('/update', marcaController.update);
 
