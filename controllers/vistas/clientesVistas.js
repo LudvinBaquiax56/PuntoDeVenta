eef4ee97d4e586c102cc80db74bbceea40a60e3b
candycx20 <candycarrillo060@gmail.com>
2023-11-14 12:43:04 -0600

    Ruta ComprasGeneral

--- controllers/vistas/clientesVistas.js
+++ controllers/vistas/clientesVistas.js
@@ -18,5 +18,18 @@ module.exports = {
       console.log(error);
       res.status(500).json({ error: 'Error al llamar al procedimiento almacenado' });
     }
+  },
+  async ComprasGeneral(req, res) {
+    try {
+      const datos = await Cliente.sequelize.query("SELECT * FROM VW_Clientes_ComprasEnGeneral;", {
+        type: QueryTypes.SELECT,
+        model: Cliente,
+      });
+      console.log(JSON.stringify(datos));
+      res.status(200).json(datos);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Error al llamar al procedimiento almacenado' });
+    }
   }
 };
--- routes/rutasProcedimientos/clientesProcRoute.js
+++ routes/rutasProcedimientos/clientesProcRoute.js
@@ -7,5 +7,6 @@ const clientesVistas = require('../../controllers/vistas/clientesVistas');
     router.get('/ComprasPorSucursal/:sucursal', clientesProcedimientos.PorSucursales);
     router.get('/DetalleCompras/:cliente', clientesProcedimientos.DetalleCompras);
     router.get('/General', clientesVistas.General);
+    router.get('/ComprasGeneral', clientesVistas.ComprasGeneral);
 
 module.exports = router;
